9665e3da3cbbef669563614b269b04eec13e2441
cor1nthian <32943000+cor1nthian@users.noreply.github.com>
2024-07-24 20:29:57 +0300

    Add files via upload

--- dbhelper.h
+++ dbhelper.h
@@ -1,6 +1,43 @@
 #ifndef _DBHELPER_H
 #define _DBHELPER_H
 
+/*       In loving memory of @pixo $oft       */
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOVIRTUALKEYCODES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOKERNEL
+#define NOMEMMGR
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NODRAWTEXT
+#define NOWINSTYLES
+#define NOSHOWWINDOW
+#define NOWINMESSAGES
+#define NOWH
+
+#include <Windows.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <sal.h>
 #include "config.h"
 #include "strhelper.h"
 
