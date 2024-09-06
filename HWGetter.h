1c33c47cebc3f9fb09a11a4fa5818a10333f3f74
cor1nthian <32943000+cor1nthian@users.noreply.github.com>
2024-09-06 08:54:46 +0300

    Add files via upload

--- HWGetter.h
+++ HWGetter.h
@@ -10,7 +10,7 @@
 const std::wstring const gc_hwEmptyval		= L"(null)";
 const std::wstring const gc_hwFriendlyName	= L"Friendly name:";
 const std::wstring const gc_hwDevInstanceID	= L"DeviceInstanceId:";
-const std::wstring const gc_hwClass           = L"Class:";
+const std::wstring const gc_hwClass			= L"Class:";
 const std::wstring const gc_hwClassGUID		= L"Class GUID:";
 
 std::vector<std::wstring> HW_GetHardDrives();
